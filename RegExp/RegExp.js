9ff39079f8d3df78d834d22ed06b4f6de6766711
dongshufeng <dsf@lejurobot.com>
2020-10-23 14:27:29 +0800

    regexp

--- RegExp/RegExp.js
+++ RegExp/RegExp.js
@@ -1,7 +1,15 @@
 const testStr = ' WAAAA BBB CW ';
-
+/**
+ * trim(str) 清除字符串开头和结尾的空格
+ * replaceSpaceAll(str) 清除字符串中所有的空格
+ * 
+ *
+ * @param {*} str
+ * @returns
+ */
 function trim(str) {
     return str.replace(/(^\s)|(\s$)/g,'');
+    // 开头 ^ 结尾 $
 }
 
 function replaceSpaceAll(str) {
@@ -10,6 +18,7 @@ function replaceSpaceAll(str) {
 
 function replaceChar(str) {
     return str.replace(/a/gi,'D');
+    // gi 并列使用即可
 }
 
 function getIndeByRegExp(str,RegExp) {
@@ -41,4 +50,5 @@ let regexp = /\d/g;
 let matchResult = str.match(regexp);
 console.log(matchResult);
 console.log(typeof [1,2,3]);
+// match returns an array of result
 console.log(Array.isArray(str.match(regexp)));
