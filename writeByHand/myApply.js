51221cbcb7a15e446bd2979ffc99307ef80afa24
dongshufeng <dsf@lejurobot.com>
2020-10-24 15:04:47 +0800

    Add my..

--- writeByHand/myApply.js
+++ writeByHand/myApply.js
@@ -2,10 +2,12 @@ Function.prototype.myApply = function(objOfMy) {
     
     objOfMy.fn = this;
     const arg = [...arguments].splice(1)[0];
+    // arg is an array for common 
    
     if(!Array.isArray(arg) && arg !== undefined) {
         throw new Error('must be an array');
     }
+
     objOfMy.fn(arg);
     
     delete objOfMy.fn;
@@ -20,3 +22,6 @@ function sayName() {
 sayName.myApply(obj);
 console.log(`------------------`);
 sayName.apply(obj);
+console.log(`TEST ABOUT APLICE(1)[0]`);
+console.log([1,2,3,4,5,6,7,8,9,10].splice(1)[0])
+// RESULT IS 2
--- writeByHand/myCall.js
+++ writeByHand/myCall.js
@@ -14,3 +14,21 @@ function sayName() {
 sayName.myCall(obj);
 console.log(`------------`);
 sayName.call(obj);
+
+console.log(`------TEST ABOUT SPLICE(1)-----`);
+console.log([1,2,3,4,5,6,7,8,9,10].splice(1));
+//  RESULT IS [2,3,4,5,6,7,8,9,10]
+/*
+
+2 首先把 `this` 赋予给我们调用的对象，给对象添加一个方法 `func` 
+
+3 解决参数问题 创建变量 `arg` 接受 `arguments` 作为一个参数修改
+
+4 我们调用的这个对象 obj 来执行新添加的函数 `func` 
+```JavaScript
+    objOfMy.func(...arg)
+```
+
+5 在运行结束之后我们删除添加给对象的这个方法
+
+*/
