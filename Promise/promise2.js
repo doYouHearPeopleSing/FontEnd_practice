adec4bce56665d83b0038bf94395f53bb00cc029
dongshufeng <dsf@lejurobot.com>
2020-10-24 20:21:56 +0800

    adjust

--- Promise/promise2.js
+++ Promise/promise2.js
@@ -2,28 +2,26 @@ const userLeft = false;
 const userWatchingCatMeme = false;
 
 
-function watchTutorialCallback(callback, errorCallback) {
-    let userLeft = false
-    let userWatchingCatMeme = false
+// function watchTutorialCallback(callback, errorCallback) {
+//     // let userLeft = false
+//     // let userWatchingCatMeme = false
   
-    if (userLeft) {
-      errorCallback({
-        name: 'User Left', 
-        message: ':('
-      })
-    } else if (userWatchingCatMeme) {
-      errorCallback({
-        name: 'User Watching Cat Meme',
-        message: 'WebDevSimplified < Cat' 
-      })
-    } else {
-      callback('Thumbs up and Subscribe')
-    }
-  }
+//     if (userLeft) {
+//       errorCallback({
+//         name: 'User Left', 
+//         message: ':('
+//       })
+//     } else if (userWatchingCatMeme) {
+//       errorCallback({
+//         name: 'User Watching Cat Meme',
+//         message: 'WebDevSimplified < Cat' 
+//       })
+//     } else {
+//       callback('Thumbs up and Subscribe')
+//     }
+//   }
   
-  function watchTutorialPromise() {
-    let userLeft = false
-    let userWatchingCatMeme = false
+  function watchTutorialPromise() {    
     return new Promise((resolve, reject) => {
       if (userLeft) {
         reject({
@@ -41,42 +39,18 @@ function watchTutorialCallback(callback, errorCallback) {
     })
   }
   
-  watchTutorialCallback(message => {
-    console.log(message)
-  }, error => {
-    console.log(error.name + ' ' + error.message)
-  })
+  // watchTutorialCallback(message => {
+  //   console.log(message)
+  // }, error => {
+  //   console.log(error.name + ' ' + error.message)
+  // })
   
-  watchTutorialPromise().then(message => {
+  watchTutorialPromise().then(message => { 
     console.log(message)
   }).catch(error => {
     console.log(error.name + ' ' + error.message)
   })
   
-  const recordVideoOne = new Promise((resolve, reject) => {
-    resolve('Video 1 Recorded')
-  })
+
+
   
-  const recordVideoTwo = new Promise((resolve, reject) => {
-    resolve('Video 2 Recorded')
-  })
-  
-  const recordVideoThree = new Promise((resolve, reject) => {
-    resolve('Video 3 Recorded')
-  })
-  
-  Promise.all([
-    recordVideoOne,
-    recordVideoTwo,
-    recordVideoThree
-  ]).then(messages => {
-    console.log(messages)
-  })
-  
-  Promise.race([
-    recordVideoOne,
-    recordVideoTwo,
-    recordVideoThree
-  ]).then(message => {
-    console.log(message)
-  })
---
+++ Promise/promise3.js
@@ -0,0 +1,27 @@
+  const recordVideoOne = new Promise((resolve, reject) => {
+    resolve('Video 1 Recorded')
+  })
+  
+  const recordVideoTwo = new Promise((resolve, reject) => {
+    resolve('Video 2 Recorded')
+  })
+  
+  const recordVideoThree = new Promise((resolve, reject) => {
+    resolve('Video 3 Recorded')
+  })
+  
+  Promise.all([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree
+  ]).then(messages => {
+    console.log(messages)
+  });
+  
+  Promise.race([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree
+  ]).then(message => {
+    console.log(message)
+  })
