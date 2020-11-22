e615c01698597c3387858904ad54b23114a487f0
dongshufeng <932230728@qq.com>
2020-11-22 17:13:38 +0800

    myTrim()

--- String/clearSpaceStr.js
+++ String/clearSpaceStr.js
@@ -1,18 +1,66 @@
 /*
  * @Author: your name
  * @Date: 2020-11-19 16:20:20
- * @LastEditTime: 2020-11-19 16:20:40
+ * @LastEditTime: 2020-11-22 17:12:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \myGitHub\String\clearSpaceStr.js
  */
-str = '    This is a       string   ';
-const strNew = str.split(' ').filter(element =>(element !== '')).join(' ');
 
-console.log(strNew);
-console.log(strNew.length);
+str = '    This is a   string   ';
 
-/**
- * 通过这个方法我们可以把词与词之间有任意长度空格的字符串
- * 转换为 词与词之间间隔一个 space 的句子 句首句尾无 space 
- */
+function myFormatSpace(string) {
+    return string.split(' ').filter(element => (element !== '')).join(' ')
+}
+
+function myClearAllSpaceA(string) {
+    return string.replace(
+        /(\s)/g,
+
+        function(all,char) {
+            return ''
+        }
+    )
+}
+
+function myClearAllSpaceB(string) {
+    return string.split(' ').filter(element => (element !== '')).join('')
+}
+
+ /**
+  * @description: 
+  * Init a function myTrim() BY using Reg
+  * HOW?
+  * ^   匹配输入的开始。如果多行标志被设置为 true，那么也匹配换行符后紧跟的位置
+
+        例如，/^A/ 并不会匹配 "an A" 中的 'A'，但是会匹配 "An E" 中的 'A'
+     
+    $   匹配输入的结束。如果多行标志被设置为 true，那么也匹配换行符前的位置
+
+        例如，/t$/ 并不会匹配 "eater" 中的 't'，但是会匹配 "eat" 中的 't'
+
+    +   匹配前面一个表达式 1 次或者多次。等价于 {1,}。
+
+        例如，/a+/ 会匹配 "candy" 中的 'a' 和 "caaaaaaandy" 中所有的 'a'，但是在 "cndy" 中不会匹配任何内容
+
+    	
+    x|y 匹配‘x’或者‘y’。
+
+        例如，/green|red/匹配“green apple”中的‘green’和“red apple”中的‘red’
+
+    
+        
+  * 
+  */ 
+
+function myTrimA(string) {
+    return string.replace(/^\s+|\s+$/g,'')
+}
+
+
+ 
+let result = myTrimA(str)
+
+console.log(myTrimA(str))
+
+console.log(result.length)
---
+++ String/nameVar.js
@@ -0,0 +1,110 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-21 13:43:57
+ * @LastEditTime: 2020-11-22 11:00:38
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \myGitHub\String\nameVar.js
+ */
+
+
+
+ function toHumpA(name) {
+    return name.replace(
+        /_(\w)/g, 
+        
+        function(all, letter){        
+            return letter.toUpperCase();
+        }
+    
+    );
+}
+
+function toHumpB(name) {
+   
+    return name
+        .split('_')
+        .map( (item) => item[0].toUpperCase() + item.substring(1) )
+        .join('');
+      
+}
+
+function toHumpC(name) {
+
+    const reArr = []
+    const arr = name.split(`_`)    
+
+    for(let i = 0; i < arr.length; i++) {
+        
+        let item = arr[i]
+
+        if(i > 0) {           
+            let pushItem = item[0].toUpperCase() + item.substring(1)
+            reArr.push(pushItem)
+        }else{
+            reArr.push(item)
+        }
+
+    }
+
+    return reArr.join('')
+}
+
+function toHumpD(name) {
+    return name.replace(
+        /_(\w)/g, 
+        
+        function(all,char){                     
+            return char.toUpperCase()
+        }
+    
+    );
+}
+
+function toLineA(name) {
+    return name.replace(/([A-Z])/g,"_$1").toLowerCase();
+}
+
+function toLineB(name) {
+    return name.replace(
+        /([A-Z])/g,
+
+        function(all,char) {
+            return `_${char.toLowerCase()}`
+        }
+
+    )
+}
+
+function allTurn(name) {
+    const arrName = name.split('')
+
+    if(arrName.includes(`_`)) {
+        return name.replace(
+            /_(\w)/g,
+
+            function(all,char) {
+                return char.toUpperCase()
+            }
+        )
+    }else{
+        return name.replace(
+            /([A-Z])/g,
+            
+            function(all,char) {
+                return `_${char.toLowerCase()}`
+            }
+        )
+    }
+}
+
+
+const str_ = 'user_name_first'
+
+const hump = `userNameFirst`
+
+console.log(allTurn(hump))
+
+ 
+
+ 
