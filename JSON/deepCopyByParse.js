f769d4fdc61397a188d1a84b787e3525d1032cc4
dongshufeng <932230728@qq.com>
2020-11-23 17:40:39 +0800

    json deepClone

--- JSON/deepCopyByParse.js
+++ JSON/deepCopyByParse.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-06 20:25:13
- * @LastEditTime: 2020-11-22 17:49:45
+ * @LastEditTime: 2020-11-23 17:39:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \myGitHub\JavaScript\deepCopy.js
@@ -35,27 +35,8 @@ function deepCloneA (obj) {
     return JSON.parse(JSON.stringify(obj))
 }
 
-function deepCloneB(target) {
-    let resultClone = {}
-    for(let key in target) {        
-        resultClone[key] = target[key]
-    }
-    return resultClone
-};
 
-function deepCloneC(target) {
-   if(typeof target === 'object') {
-       let  resultClone = Array.isArray(target) ? []:{}
-       for(const key in target) {
-           resultClone[key] = deepCloneC(target[key])
-       }
-       return resultClone
-   } else {
-       return target
-   }
-}
-
-const objCopy = deepCloneC(objOrign)
+const objCopy = deepCloneA(objOrign)
 
 /**
  * @description: 
@@ -75,8 +56,9 @@ console.log(typeof objCopy)
  * @description: 
  * COPY AN ARRAY
  */
+console.log(`-------------------------------------NEXT------------------------------------------`)
 const arrOr = [1,2,3,4,{ id:5}]
-const arrCo = deepCloneC(arrOr)
+const arrCo = deepCloneA(arrOr)
 
 arrOr.push(6)
 arrCo[4].id = 'fuckYou'
