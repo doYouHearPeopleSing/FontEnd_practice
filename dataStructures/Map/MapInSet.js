209948ac9f55fb8ace879cb16dce561fdbaaaf02
dongshufeng <932230728@qq.com>
2020-11-23 18:39:02 +0800

    Format and rename

--- dataStructures/Map/MapInSet.js
+++ dataStructures/Map/MapInSet.js
@@ -1,74 +1,80 @@
-let myMap = new Map();
+/*
+ * @Author: your name
+ * @Date: 2020-08-27 10:04:37
+ * @LastEditTime: 2020-11-23 18:29:48
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \myGitHub\dataStructures\Map\MapInSet.js
+ */
 
-// let keyObj = {};
-// let keyFun = function() {};
-// let keyStirng = 'a string';
+ 
+let myMap = new Map()
 
-myMap.set(1,'one').set(2,'two').set(3,'three');
+myMap.set(1,'one').set(2,'two').set(3,'three') 
 
-console.log(myMap.size);
-console.log(myMap);
-// myMap.clear();
-console.log(myMap);
+console.log(myMap.size) 
+console.log(myMap) 
+// myMap.clear() 
+console.log(myMap) 
 
 
 // for (let [key, value] of myMap) {
-//     console.log(key + " = " + value);
+//     console.log(key + " = " + value) 
 //   }
 //   // 将会显示两个log。一个是"0 = zero"另一个是"1 = one"
   
 
-  console.log(`--------`);
+  console.log(`--------`) 
   for (let key of myMap.keys()) {
-    console.log(key);
+    console.log(key) 
   }
-  console.log(`--------`);
+  console.log(`--------`) 
   // 将会显示两个log。 一个是 "0" 另一个是 "1"
   
   for (let value of myMap.values()) {
-    console.log(value);
+    console.log(value) 
   }
-  console.log(`--------`);
+  console.log(`--------`) 
   // 将会显示两个log。 一个是 "zero" 另一个是 "one"
   
   for (let [key, value] of myMap.entries()) {
-    console.log(key + " = " + value);
+    console.log(key + " = " + value) 
   }
 //   // 将会显示两个log。 一个是 "0 = zero" 另一个是 "1 = one"
-//   console.log(`--------`);
+//   console.log(`--------`) 
 //   myMap.forEach(function(value, key) {
-//     console.log(key + " = " + value);
+//     console.log(key + " = " + value) 
 //   })
 
   let first = new Map([
     [1, 'one'],
     [2, 'two'],
     [3, 'three'],
-  ]);
+  ]) 
   
   let second = new Map([
     [1, 'uno'],
     [2, 'dos']
-  ]);
+  ]) 
   
   // 合并两个Map对象时，如果有重复的键值，则后面的会覆盖前面的。
   // 展开运算符本质上是将Map对象转换成数组。
-  let merged = new Map([...first]);
+  let merged = new Map([...first]) 
   
-//   console.log(merged.get(1)); // uno
-//   console.log(merged.get(2)); // dos
-//   console.log(merged.get(3)); // three
+//   console.log(merged.get(1))  // uno
+//   console.log(merged.get(2))  // dos
+//   console.log(merged.get(3))  // three
 
-// //   merged.delete(1);
+// //   merged.delete(1) 
 
-//   console.log(merged);
-//   console.log(`****`);
+//   console.log(merged) 
+//   console.log(`****`) 
 
 //   merged.forEach(function(value) {
-//       console.log(`${value}`);
-//   });
+//       console.log(`${value}`) 
+//   }) 
 
-console.log('+++++');
-console.log(myMap.has(2));
-myMap.set(2,'二');
-console.log(myMap.get(2));
+console.log('+++++') 
+console.log(myMap.has(2)) 
+myMap.set(2,'二') 
+console.log(myMap.get(2)) 
--- dataStructures/Map/map.js
+++ dataStructures/Map/map.js
@@ -1,18 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-26 15:10:03
+ * @LastEditTime: 2020-11-23 18:28:51
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \myGitHub\dataStructures\Map\map.js
+ */
+
+ 
 function defaultToString (item) {
     if(item === null) {
-        return 'NULL';
+        return 'NULL' 
     } else if (item === undefined) {
-        return 'UNDEFINED';
+        return 'UNDEFINED' 
     } else if (typeof item === 'string' || item instanceof String) {
         return `${item}`
     } 
-    return item.toString();
+    return item.toString() 
 }
 
 class ValuePair {
     constructor(key,value) {
-        this.key = key;
-        this.value = value;
+        this.key = key 
+        this.value = value 
     }
     toString() {
         return `[#${this.key}:${this.value}]`
@@ -21,46 +31,46 @@ class ValuePair {
 
 class Dictionary {
     constructor(toStrFn = defaultToString) {
-        this.toStrFn = toStrFn;
-        this.table = {};
+        this.toStrFn = toStrFn 
+        this.table = {} 
     }
     hasKey(key) {
-        return this.table[this.toStrFn(key)] != null;
+        return this.table[this.toStrFn(key)] != null 
     }
     set(key,value) {
         if(key != null && value != null) {
-            const tableKey = this.toStrFn(key);
-            this.table[tableKey] = new ValuePair(key,value);
-            return true;
+            const tableKey = this.toStrFn(key) 
+            this.table[tableKey] = new ValuePair(key,value) 
+            return true 
         }
-        return false;
+        return false 
     }
 
     get(key) {
         if ( this.hasKey(key) ) {
-            return this.table[this.toStrFn(key)];
+            return this.table[this.toStrFn(key)] 
         }
-        return undefined;
+        return undefined 
     }
 
     size() {
-        return Object.keys(this.table).length;
+        return Object.keys(this.table).length 
     }
 
     keyValues() {
-        return Object.values(this.table);
+        return Object.values(this.table) 
     }
 }
 
-const dictionary = new Dictionary();
-dictionary.set('a','A is for Apple');
-dictionary.set('b','B is for banana');
+const dictionary = new Dictionary() 
+dictionary.set('a','A is for Apple') 
+dictionary.set('b','B is for banana') 
 
-// console.log(dictionary);
-console.log(dictionary.hasKey('a'));
+// console.log(dictionary) 
+console.log(dictionary.hasKey('a')) 
 
-console.log(dictionary.get('a'));
+console.log(dictionary.get('a')) 
 
-console.log(dictionary.size());
+console.log(dictionary.size()) 
 
-console.log(dictionary.keyValues());
+console.log(dictionary.keyValues()) 
