c1df73f6756006357c8a921ab20c93ae1ac839e0
dongshufeng <932230728@qq.com>
2020-12-03 17:12:37 +0800

    add sort

--- Sort/sortBySort.js
+++ Sort/sortBySort.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-11-23 20:15:03
- * @LastEditTime: 2020-11-23 20:17:26
+ * @LastEditTime: 2020-12-03 16:58:12
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \myGitHub\Sort\sortBySort.js
@@ -11,7 +11,7 @@
  const arrayTest = [10,9,8,7,6,5,4,3,2,1,0]
 
  arrayTest.sort(
-     (a,b) => {return a - b}     
+     (a,b) => a - b
  )
 
  console.log(arrayTest)
